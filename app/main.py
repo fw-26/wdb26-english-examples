f5b84237d5dc22a6f8db87b2467b1a3832928179
welandfr <welandfr@arcada.fi>
2026-03-25 15:40:37 +0200

    what is my ip

--- app/main.py
+++ app/main.py
@@ -1,12 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
+my_name = "Fredde"
+
+# Main route for this API
 @app.get("/")
-def read_root():
-    return { "msg": "Final version!" }
+def read_root(): 
+    # f-string concatenation
+    return { "msg": f"Hello {my_name}"}
+
+# What is my ip 
+@app.get("/api/ip")
+def api_ip(request: Request): 
+    # f-string concatenation
+    return { "ip": request.client.host }
+
+@app.get("/ip", response_class=HTMLResponse)
+def html_ip(request: Request):
+    return f"<h1>Your IP is {request.client.host}</h1>"
 
 
-@app.get("/items/{id}")
-def read_item(item_id: int, q: str = None):
-    return {"id": id, "q": q}
+
+
